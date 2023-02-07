b3d685e39cf255ccb213614126e9652ed0ada606
usmangq12 <usmangq12@gmail.com>
2023-02-07 17:17:45 +0500

    navbar component with in the navigation container

--- App.tsx
+++ App.tsx
@@ -3,15 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home, AddNote } from "./screens";
 import { NavBar } from "./components";
-import { AppContext } from "./context/appContext";
+import { AppContext } from "./context/AppContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <AppContext>
-      <NavBar />
       <NavigationContainer>
+        <NavBar />
         <Stack.Navigator
           initialRouteName="Home"
           screenOptions={{
